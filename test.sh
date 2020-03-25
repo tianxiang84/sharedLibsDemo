1317a2f3a41637324c865ba17af15ecf358a7703
tianxiang84 <tsu@slb.com>
2020-03-25 10:12:10 -0400

    Windows saving

--- test.sh
+++ test.sh
@@ -1,20 +1,26 @@
+On Linux:
+- Separated folders:
 g++ -c -Wall -fPIC -o obj/shared.o src/shared/shared.cpp
 g++ -shared -Wl,-soname,libshared.so -o lib/libshared.so obj/shared.o
 g++ -c -Wall -o obj/main.o src/main/main.cpp
-
-g++ -o bin/main src/main/main.cpp lib/libshared.so -Wl,-rpath=lib
-g++ -o bin/main src/main/main.cpp -Llib -lshared -Wl,-rpath=lib
-
-g++ -o -Llib -lshared bin/main src/main/main.cpp -Wl,-rpath=lib
+g++ -o bin/main src/main/main.cpp -Llib -lshared -Wl,-rpath=lib (OR g++ -o bin/main src/main/main.cpp lib/libshared.so -Wl,-rpath=lib)
 ./bin/main
 
-
-
-
-
-
-
+- Same folder:
 g++ -fPIC -shared shared.cpp -o libshared.so &&
-g++ -c -Wall -o main.o main.cpp &&
 g++ -o main main.cpp -L. -lshared -Wl,-rpath,. &&
 ./main 
+
+
+On Windows (mingw-w64)
+- Separated folders:
+g++ -c -Wall -fPIC -o obj\shared.o src\shared\shared.cpp
+g++ -shared -Wl,-soname,libshared.dll -o bin\libshared.dll obj\shared.o 
+g++ -c -Wall -o obj\main.o src\main\main.cpp
+g++ -o bin\main.exe obj\main.o -Lbin -lshared -Wl,-rpath=bin (OR g++ -o bin\main obj/main.o bin\libshared.dll -Wl,-rpath=bin)
+.\bin\main.exe
+
+- Same folder:
+g++ -fPIC -shared shared.cpp -o libshared.dll
+g++ -o main.exe main.cpp -L. -lshared -Wl,-rpath,.
+./main.exe 
